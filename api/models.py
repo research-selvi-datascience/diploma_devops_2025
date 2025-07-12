14834aada0ad01b93668a5626e5654f5026cbecb
Selvi Esther <Selvi.digitalsolutions@gmail.com>
2025-07-12 23:51:14 +0100

    feat: added makemigrations in workflows and changes in api/models

--- api/models.py
+++ api/models.py
@@ -4,4 +4,5 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     author = models.CharField(max_length=255)
+    new_field = models.CharField(max_length=255, default='new_field')
     created_at = models.DateTimeField(auto_now_add=True)
