675e8efccde7ea42f7ad6c3184a665b30645e399
Selvi Esther <Selvi.digitalsolutions@gmail.com>
2025-07-12 02:13:20 +0100

    feat: added new endpoint for book creation

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,15 +1,26 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
+from api.models import Book
 
 class BookViewTest(APITestCase):
     
     def test_response_is_correct(self):
+        book = Book.objects.create(
+            title="Demo",
+            description="Description",
+            author ="Author"
+        )
         url = reverse('api:books')
         response = self.client.get(url, format = 'json')
         assert response.status_code == status.HTTP_200_OK
         body = response.json()
-        assert body['hello'] == 'django'
+        returned_book = body[0]
+        assert returned_book["title"] == book.title
+        assert returned_book["description"] == book.description
+        assert returned_book["author"] == book.author
+        
+        
         
 class HealthViewTest(APITestCase):
     
